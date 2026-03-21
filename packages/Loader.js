5afdc7b0e1d8836c0e83fad0a10a6e3a48aadd71
AhmedTalbii <ahmedtalbi459@gmail.com>
2026-03-21 00:10:02 +0000

    first commit + landing page

---
+++ packages/Loader.js
@@ -0,0 +1,47 @@
+export class Loader {
+    constructor(app) {
+        this.app = app;
+    }
+
+    async LoadPageStyle(path, page) {
+        path += page + "/" + page + ".css";
+        try {
+            const res = await fetch(path);
+            const css = await res.text();
+            const style = document.createElement("style");
+            style.dataset.style = true;
+            style.textContent = css;
+            document.querySelectorAll("[data-style]").forEach(e => e.remove());
+            document.head.appendChild(style);
+        } catch { return null; }
+    }
+
+    async LoadPageScript(path, page) {
+        path += page + "/" + page + ".js";
+        try {
+            const res = await fetch(path);
+            const js = await res.text();
+            const script = document.createElement("script");
+            script.dataset.script = true;
+            script.textContent = js;
+            document.querySelectorAll("[data-script]").forEach(e => e.remove());
+            document.body.appendChild(script);
+        } catch { return null; }
+    }
+
+    async LoadPageHtml(path, page) {
+        path += page + "/" + page + ".html";
+        try {
+            const res = await fetch(path);
+            const html = await res.text();
+            this.app.innerHTML = html;
+            return html;
+        } catch { return null; }
+    }
+
+    async LoadPage(page, path = "/src/pages/") {
+        await this.LoadPageStyle(path, page);
+        await this.LoadPageHtml(path, page);
+        await this.LoadPageScript(path, page);
+    }
+}
---
+++ src/app.js
@@ -0,0 +1,12 @@
+import { Loader } from "../packages/Loader.js";
+
+const app = document.getElementById("app");
+
+const PageLoader = new Loader(app);
+
+let auth = false;
+if (auth) {
+    app.textContent = "authenticated"
+} else {
+    PageLoader.LoadPage("login");
+}
